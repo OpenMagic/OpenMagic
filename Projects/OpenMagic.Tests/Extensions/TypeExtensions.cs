01095bcbcd151cf0fce6dbe88a0df8f52b2b9334
Tim Murphy <tim@26tp.com>
2013-03-11 07:38:45 +1100

    New IsString().

---
+++ Projects/OpenMagic.Tests/Extensions/TypeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenMagic.Extensions;
+
+namespace OpenMagic.Tests.Extensions {
+    [TestClass]
+    public class TypeExtensions {
+
+        [TestClass]
+        public class IsString {
+
+            [TestMethod]
+            public void ReturnsTrueWhenTypeIsString() {
+                typeof(string).IsString().Should().BeTrue();
+            }
+
+            [TestMethod]
+            public void ReturnsFalseWhenTypeIsNotString() {
+                typeof(int).IsString().Should().BeFalse();
+                typeof(DateTime).IsString().Should().BeFalse();                
+            }
+        }
+    }
+}
---
+++ Projects/OpenMagic/Extensions/TypeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OpenMagic.Extensions {
+    public static class TypeExtensions {
+
+        /// <summary>
+        /// Return true if the type is a string.
+        /// </summary>
+        /// <param name="value">The type to test for string.</param>
+        /// <remarks>
+        /// Syntactic sugar.
+        /// </remarks>
+        public static bool IsString(this Type value) {
+            return value == typeof(string);
+        }
+    }
+}
