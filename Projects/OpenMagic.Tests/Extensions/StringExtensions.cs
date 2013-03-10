11a8c2393d116aa3f137fb5307b4f7edf93a0b55
Tim Murphy <tim@26tp.com>
2013-03-11 08:17:28 +1100

    New IsNullOrWhiteSpace().

---
+++ Projects/OpenMagic.Tests/Extensions/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenMagic.Extensions;
+
+namespace OpenMagic.Tests.Extensions {
+    [TestClass]
+    public class StringExtensions {
+
+        [TestClass]
+        public class IsNullOrWhiteSpace {
+
+            [TestMethod]
+            public void ReturnsTrueWhenValueIsNullOrWhiteSpace() {
+                OpenMagic.Extensions.StringExtensions.IsNullOrWhiteSpace(null).Should().BeTrue();
+                OpenMagic.Extensions.StringExtensions.IsNullOrWhiteSpace("").Should().BeTrue();
+                OpenMagic.Extensions.StringExtensions.IsNullOrWhiteSpace(" ").Should().BeTrue();
+            }
+
+            [TestMethod]
+            public void ReturnsFalseWhenValueIsNotNullOrWhiteSpace() {
+                OpenMagic.Extensions.StringExtensions.IsNullOrWhiteSpace("a").Should().BeFalse();
+                OpenMagic.Extensions.StringExtensions.IsNullOrWhiteSpace(" a").Should().BeFalse();
+                OpenMagic.Extensions.StringExtensions.IsNullOrWhiteSpace("a ").Should().BeFalse();
+            }
+        }
+    }
+}
---
+++ Projects/OpenMagic/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenMagic.Extensions {
+    public static class StringExtensions {
+
+        /// <summary>
+        /// Indicates whether a specified string is null, empty, or consists only of white-space characters.
+        /// </summary>
+        /// <param name="value">The value to test.</param>
+        /// <remarks>
+        /// Syntactic sugar.
+        /// </remarks>
+        public static bool IsNullOrWhiteSpace(this string value) {
+            return string.IsNullOrWhiteSpace(value);
+        }
+    }
+}
