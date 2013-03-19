64dab0488956d6a6424020f9d854bd13ad5c8f68
Tim Murphy <tim@26tp.com>
2013-03-20 00:52:16 +1100

    New OpenMagic.Extensions.Collections.Generic.IEnumerableExtensions.

---
+++ Projects/OpenMagic.Tests/Extensions/Collections/Generic/IEnumerableExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenMagic.Extensions.Collections.Generic;
+
+namespace OpenMagic.Tests.Extensions.Collections.Generic {
+    [TestClass]
+    public class IEnumerableExtensions {
+
+        [TestClass]
+        public class IsNullOrEmpty {
+
+            [TestMethod]
+            public void ReturnsTrueWhenValueIsNull() {
+
+                IEnumerable<string> nullString = null;
+                IEnumerable<int> nullInt = null;
+
+                OpenMagic.Extensions.Collections.Generic.IEnumerableExtensions.IsNullOrEmpty(nullString).Should().BeTrue();
+                OpenMagic.Extensions.Collections.Generic.IEnumerableExtensions.IsNullOrEmpty(nullInt).Should().BeTrue();
+            }
+
+            [TestMethod]
+            public void ReturnsTrueWhenValueIsHasZeroElements() {
+
+                var emptyString = new List<string>();
+                var emptyInt = new List<int>();
+
+                OpenMagic.Extensions.Collections.Generic.IEnumerableExtensions.IsNullOrEmpty(emptyString).Should().BeTrue();
+                OpenMagic.Extensions.Collections.Generic.IEnumerableExtensions.IsNullOrEmpty(emptyInt).Should().BeTrue();
+            }
+
+            [TestMethod]
+            public void ReturnsFalseWhenValueHasElements() {
+
+                var enumerableString = new List<string>() {"a", "b"};
+                var enumerableInt = new List<int>() {1,2};
+
+                OpenMagic.Extensions.Collections.Generic.IEnumerableExtensions.IsNullOrEmpty(enumerableString).Should().BeFalse();
+                OpenMagic.Extensions.Collections.Generic.IEnumerableExtensions.IsNullOrEmpty(enumerableInt).Should().BeFalse();
+            }
+        }
+    }
+}
---
+++ Projects/OpenMagic/Extensions/Collections/Generic/IEnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenMagic.Extensions.Collections.Generic {
+    public static class IEnumerableExtensions {
+
+        /// <summary>
+        /// Indicates whether a specified enumerable is null or empty.
+        /// </summary>
+        /// <param name="value">The value to test.</param>
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> value) {
+            return value == null || !value.Any();
+        }
+    }
+}
