d3f37803f49dd6591aef281ae3d88e5fe732a677
Tim Murphy <tim@26tp.com>
2013-04-05 15:49:05 +1100

    New StringExtensions.ToLines().

--- Projects/OpenMagic.Tests/Extensions/StringExtensionsTests.cs
+++ Projects/OpenMagic.Tests/Extensions/StringExtensionsTests.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System.Linq;
+using System.Text;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenMagic.Extensions;
 
@@ -24,6 +26,84 @@ namespace OpenMagic.Tests.Extensions
                 StringExtensions.IsNullOrWhiteSpace("a").Should().BeFalse();
                 StringExtensions.IsNullOrWhiteSpace(" a").Should().BeFalse();
                 StringExtensions.IsNullOrWhiteSpace("a ").Should().BeFalse();
+            }
+        }
+
+        [TestClass]
+        public class ToLines
+        {
+            [TestMethod]
+            public void ReturnsLinesWhenValueHasLines()
+            {
+                // Given
+                var sb = new StringBuilder();
+
+                sb.AppendLine("line 1");
+                sb.AppendLine("line 2 with trailing space ");
+                sb.AppendLine();
+                sb.AppendLine(" line 4 with leading space");
+                sb.AppendLine();
+
+                // When
+                var lines = sb.ToString().ToLines();
+
+                // Then
+                lines.Count().Should().Be(5);
+                lines.ElementAt(0).Should().Be("line 1");
+                lines.ElementAt(1).Should().Be("line 2 with trailing space ");
+                lines.ElementAt(2).Should().Be("");
+                lines.ElementAt(3).Should().Be(" line 4 with leading space");
+                lines.ElementAt(4).Should().Be("");
+            }
+
+            [TestMethod]
+            public void ReturnsTrimmedLinesWhenValueHasLinesAndTrimLinesIsTrue()
+            {
+                // Given
+                var sb = new StringBuilder();
+
+                sb.AppendLine("line 1");
+                sb.AppendLine("line 2 with trailing space trimmed ");
+                sb.AppendLine();
+                sb.AppendLine(" line 4 with leading space trimmed");
+                sb.AppendLine();
+
+                // When
+                var lines = sb.ToString().ToLines(true);
+
+                // Then
+                lines.Count().Should().Be(5);
+                lines.ElementAt(0).Should().Be("line 1");
+                lines.ElementAt(1).Should().Be("line 2 with trailing space trimmed");
+                lines.ElementAt(2).Should().Be("");
+                lines.ElementAt(3).Should().Be("line 4 with leading space trimmed");
+                lines.ElementAt(4).Should().Be("");
+            }
+
+            [TestMethod]
+            public void ReturnsZeroLinesWhenValueIsEmptyString()
+            {
+                // Given
+                string value = "";
+
+                // When
+                var lines = value.ToLines();
+
+                // Then
+                lines.Count().Should().Be(0);
+            }
+
+            [TestMethod]
+            public void ReturnsZeroLinesWhenValueIsNull()
+            {
+                // Given
+                string value = null;
+
+                // When
+                var lines = value.ToLines();
+
+                // Then
+                lines.Count().Should().Be(0);
             }
         }
     }
--- Projects/OpenMagic/Extensions/StringExtensions.cs
+++ Projects/OpenMagic/Extensions/StringExtensions.cs
@@ -1,4 +1,8 @@
-﻿namespace OpenMagic.Extensions
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace OpenMagic.Extensions
 {
     public static class StringExtensions
     {
@@ -12,6 +16,51 @@
         public static bool IsNullOrWhiteSpace(this string value)
         {
             return string.IsNullOrWhiteSpace(value);
+        }
+
+        /// <summary>
+        /// Splits a string value into lines.
+        /// </summary>
+        /// <param name="value">The string to split into lines.</param>
+        public static IEnumerable<string> ToLines(this string value)
+        {
+            return value.ToLines(false);
+        }
+
+        /// <summary>
+        /// Splits a string value into lines.
+        /// </summary>
+        /// <param name="value">The string to split into lines.</param>
+        /// <param name="trimLines">If true the lines are trimmed.</param>
+        public static IEnumerable<string> ToLines(this string value, bool trimLines)
+        {
+            if (value == null)
+            {
+                return Enumerable.Empty<string>();
+            }
+
+            var lines = new List<string>();
+            var reader = new StringReader(value);
+
+            while (true)
+            {
+                var line = reader.ReadLine();
+
+                if (line == null)
+                {
+                    break;
+                }
+
+                if (trimLines)
+                {
+                    lines.Add(line.Trim());
+                }
+                else
+                {
+                    lines.Add(line);
+                }
+            }
+            return lines;
         }
     }
 }
