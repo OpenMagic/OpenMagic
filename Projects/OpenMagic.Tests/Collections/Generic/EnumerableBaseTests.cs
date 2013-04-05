87cc926af76b142dc8b663b6474bc84a55786ade
Tim Murphy <tim@26tp.com>
2013-04-05 17:05:35 +1100

    New EnumerableBase<T>.

---
+++ Projects/OpenMagic.Tests/Collections/Generic/EnumerableBaseTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenMagic.Collections.Generic;
+using FluentAssertions;
+
+namespace OpenMagic.Tests.Collections.Generic
+{
+    public class EnumerableBaseTests
+    {
+        [TestClass]
+        public class Constructor
+        {
+            [TestMethod]
+            public void CanConstruct()
+            {
+                // When
+                var collection = new EnumerableBaseImplementation();
+
+                // Then
+                collection.Should().NotBeNull();
+            }
+        }
+
+        [TestClass]
+        public class GetEnumerator
+        {
+            [TestMethod]
+            public void CanEnumerateAllValues()
+            {
+                // Given
+                var collection = new EnumerableBaseImplementation();
+
+                // When
+                var enumerator = collection.GetEnumerator();
+
+                // Then
+                var i = -1;
+                while (enumerator.MoveNext())
+                {
+                    enumerator.Current.Should().Be(++i, "because collection should be 0 ... 9");
+                }
+
+                i.Should().Be(9, "because the last item in collection should be 9");
+            }
+        }
+
+        public class EnumerableBaseImplementation : EnumerableBase<int>
+        {
+            public EnumerableBaseImplementation()
+                : base()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    this.Items.Add(i);
+                }
+            }
+        }
+    }
+}
---
+++ Projects/OpenMagic/Collections/Generic/EnumerableBase`1.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace OpenMagic.Collections.Generic
+{
+    /// <summary>
+    /// Provides a base class for classes that need to implement <see cref="IEnumerable<T>"/>.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of elements in the collection.
+    /// </typeparam>
+    public abstract class EnumerableBase<T> : IEnumerable<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of the Collection<T> class that is empty.
+        /// </summary>
+        public EnumerableBase()
+        {
+            Items = new List<T>();
+        }
+
+        /// <summary>
+        /// Gets the items this class can enumerate
+        /// </summary>
+        protected IList<T> Items { get; set; }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection. 
+        /// </summary>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Items.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns a non-generic enumerator that iterates through the collection.
+        /// </summary>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
