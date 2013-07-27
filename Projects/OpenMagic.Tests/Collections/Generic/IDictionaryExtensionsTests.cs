22be68f856ec20a74eed04ed733e6e82075a8d67
Tim Murphy <tim@26tp.com>
2013-07-27 14:55:25 +1000

    New IDictionaryExtensions.FindValue<TKey, TValue>(key);

---
+++ Projects/OpenMagic.Tests/Collections/Generic/IDictionaryExtensionsTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenMagic.Collections.Generic;
+
+namespace OpenMagic.Tests.Collections.Generic
+{
+    public class IDictionaryExtensionsTests
+    {
+        [TestClass]
+        public class FindValue
+        {
+            [TestMethod]
+            public void ShouldReturnValueIfKeyIsInDictionary()
+            {
+                // Given
+                var dictionary = new Dictionary<int, string>();
+
+                dictionary.Add(1, "a");
+                dictionary.Add(2, "b");
+
+                // When / Then
+                dictionary.FindValue(1).Should().Be("a");
+                dictionary.FindValue(2).Should().Be("b");
+            }
+
+            [TestMethod]
+            public void ShouldReturnNullIfKeyIsNotDictionary()
+            {
+                // Given
+                var dictionary = new Dictionary<int, string>();
+
+                dictionary.Add(1, "a");
+
+                // When / Then
+                dictionary.FindValue(2).Should().BeNull();
+            }
+
+            [TestMethod]
+            public void ShouldThrowArgumentNullExceptionWhen_key_IsNull()
+            {
+                // Given
+                var dictionary = new Dictionary<string, string>();
+
+                // When
+                Action action = () => dictionary.FindValue(null);
+
+                // Then
+                action.ShouldThrow<ArgumentNullException>().WithMessage(string.Format("Value cannot be null.\r\nParameter name: {0}", "key"));
+            }
+        }
+    }
+}
---
+++ Projects/OpenMagic/Collections/Generic/IDictionaryExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OpenMagic.Collections.Generic
+{
+    public static class IDictionaryExtensions
+    {
+        /// <summary>
+        /// Finds the value associated with the specified key.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+        /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+        /// <param name="dictionary">The dictionary to search.</param>
+        /// <param name="key">The key of the value to find.</param>
+        /// <returns>The value associated with the specified key, if the key is found; otherwise, the default value for the type of the value parameter.</returns>
+        public static TValue FindValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            key.MustNotBeNull("key");
+
+            TValue value;
+
+            dictionary.TryGetValue(key, out value);
+
+            return value;
+        }
+    }
+}
