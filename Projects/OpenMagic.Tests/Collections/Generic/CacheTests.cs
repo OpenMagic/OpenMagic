d4fa0b5a1019474290d1d09c83ac606b763d172e
Tim Murphy <tim@26tp.com>
2013-09-04 05:22:36 +1000

    New Cache<TKey, TValue>.

---
+++ Projects/OpenMagic.Tests/Collections/Generic/CacheTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenMagic.Collections.Generic;
+
+namespace OpenMagic.Tests.Collections.Generic
+{
+    public class CacheTests
+    {
+        [TestClass]
+        public class Get
+        {
+            [TestMethod]
+            public void ShouldReturnResultOfValueFactoryWhenTypeIsNotInCache()
+            {
+                // Given
+                var cache = new Cache<string, string>();
+
+                // When
+                var value = cache.Get("a", () => "value");
+
+                // Then
+                value.Should().Be("value");
+            }
+
+            [TestMethod]
+            public void ShouldReturnResultValueFromDictionaryWhenTypeIsInCache()
+            {
+                // Given
+                var cache = new Cache<Type, Exception>();
+                var initialValue = cache.Get(typeof(ArgumentNullException), () => new ArgumentNullException());
+                                
+                // When
+                var value = cache.Get(typeof(ArgumentNullException), () => new ArgumentNullException());
+
+                // Then
+                value.Should().BeSameAs(initialValue);
+            }
+        }
+    }
+}
