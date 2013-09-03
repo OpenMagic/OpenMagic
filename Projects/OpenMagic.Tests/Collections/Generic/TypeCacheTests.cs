60cd27792d5b090cbb095172118d5031dc73dbab
Tim Murphy <tim@26tp.com>
2013-09-04 05:31:42 +1000

    New TypeCache<TValue>.

---
+++ Projects/OpenMagic.Tests/Collections/Generic/TypeCacheTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenMagic.Collections.Generic;
+
+namespace OpenMagic.Tests.Collections.Generic
+{
+    public class TypeCacheTests
+    {
+        [TestClass]
+        public class Get
+        {
+            [TestMethod]
+            public void ShouldReturnResultOfValueFactoryWhenTypeIsNotInCache()
+            {
+                // Given
+                var cache = new TypeCache<object>();
+
+                // When
+                var value = cache.Get<string>(() => "value");
+
+                // Then
+                value.Should().Be("value");
+            }
+
+            [TestMethod]
+            public void ShouldReturnResultValueFromDictionaryWhenTypeIsInCache()
+            {
+                // Given
+                var cache = new TypeCache<Exception>();
+                var initialValue = cache.Get<ArgumentNullException>(() => new ArgumentNullException());
+                                
+                // When
+                var value = cache.Get<ArgumentNullException>(() => new ArgumentNullException());
+
+                // Then
+                value.Should().BeSameAs(initialValue);
+            }
+        }
+    }
+}
---
+++ Projects/OpenMagic/Collections/Generic/TypeCache`1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OpenMagic.Collections.Generic
+{
+    /// <summary>
+    /// Represents a cache where the key is <see cref="Type"/>.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value in the cache.</typeparam>
+    public class TypeCache<TValue> : Cache<Type, TValue>
+    {
+        /// <summary>
+        /// Gets the value for <typeparamref name="TType"/> from the cache. If <typeparamref name="TKey"/> is not in cache
+        /// then the result of <paramref name="valueFactory"/> is added to the cache and returned.
+        /// </summary>
+        /// <param name="valueFactory">The function to invoke if <typeparamref name="TType"/> is not in the cache.</param>
+        public TValue Get<TType>(Func<TValue> valueFactory)
+        {
+            return base.Get(typeof(TType), valueFactory);
+        }
+    }
+}
