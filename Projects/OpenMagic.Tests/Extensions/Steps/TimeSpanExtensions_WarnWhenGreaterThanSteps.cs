f3f969595858476da05de96fd88b73f9e40ef069
Tim Murphy <tim@26tp.com>
2014-05-05 20:41:42 +1000

    New TimeSpanExtensions.WarnWhenGreaterThan(timespan, maximumTimeSpan, messageFormat).

---
+++ Projects/OpenMagic.Tests/Extensions/Steps/TimeSpanExtensions_WarnWhenGreaterThanSteps.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Common.Logging;
+using FakeItEasy;
+using FluentAssertions;
+using OpenMagic.Extensions;
+using TechTalk.SpecFlow;
+
+namespace OpenMagic.Tests.Extensions.Steps
+{
+    [Binding]
+    // ReSharper disable once InconsistentNaming
+    public class TimeSpanExtensions_WarnWhenGreaterThanSteps
+    {
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+
+        private readonly ILog FakeLog;
+        private object[] ActualArgs;
+        private string ActualFormat;
+
+        private double GivenElapsedMilliseconds;
+        private int GivenMaximumMilliseconds;
+        private string GivenWarningMessageTemplate;
+
+        public TimeSpanExtensions_WarnWhenGreaterThanSteps()
+        {
+            FakeLog = A.Fake<ILog>();
+            A.CallTo(() => FakeLog.WarnFormat(A<string>.Ignored, A<object[]>.Ignored))
+                .Invokes((string format, object[] args) =>
+                {
+                    ActualFormat = format;
+                    ActualArgs = args;
+
+                    // Send to real loggers.
+                    Log.WarnFormat(ActualFormat, ActualArgs);
+                });
+        }
+
+        [Given(@"elapsed time is '(.*)' milliseconds")]
+        public void GivenElapsedTimeIsMilliseconds(int elapsedMilliseconds)
+        {
+            GivenElapsedMilliseconds = elapsedMilliseconds;
+        }
+
+        [Given(@"maximum is '(.*)' milliseconds")]
+        public void GivenMaximumIsMilliseconds(int maximumMilliseconds)
+        {
+            GivenMaximumMilliseconds = maximumMilliseconds;
+        }
+
+        [Given(@"warning message template is '(.*)'")]
+        public void GivenWarningMessageTemplateIs(string givenWarningMessageTemplate)
+        {
+            GivenWarningMessageTemplate = givenWarningMessageTemplate;
+        }
+
+        [When(@"WarnWhenGreaterThan\(stopwatch, message\) is called")]
+        public void WhenWarnWhenGreaterThanStopwatchMessageIsCalled()
+        {
+            var elapsed = TimeSpan.FromMilliseconds(GivenElapsedMilliseconds);
+
+            elapsed.WarnWhenGreaterThan(TimeSpan.FromMilliseconds(GivenMaximumMilliseconds), GivenWarningMessageTemplate, FakeLog);
+        }
+
+        [Then(@"a warning is not logged")]
+        public void ThenAWarningIsNotLogged()
+        {
+            A.CallTo(() => FakeLog.WarnFormat(A<string>.Ignored, A<object>.Ignored)).MustHaveHappened(Repeated.Never);
+        }
+
+        [Then(@"'(.*)' is added to the log")]
+        public void ThenIsAddedToTheLog(string expectedWarning)
+        {
+            string.Format(ActualFormat, ActualArgs).Should().Be(expectedWarning);
+        }
+    }
+}
---
+++ Projects/OpenMagic/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Common.Logging;
+
+namespace OpenMagic.Extensions
+{
+    public static class TimeSpanExtensions
+    {
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+
+        public static void WarnWhenGreaterThan(this TimeSpan timespan, TimeSpan maximumTimeSpan, string messageFormat)
+        {
+            timespan.WarnWhenGreaterThan(maximumTimeSpan, messageFormat, Log);
+        }
+
+        public static void WarnWhenGreaterThan(this TimeSpan timespan, TimeSpan maximumTimeSpan, string messageFormat, ILog log)
+        {
+            if (timespan < maximumTimeSpan)
+            {
+                return;
+            }
+
+            if (messageFormat.Contains("{milliseconds}", StringComparison.InvariantCultureIgnoreCase))
+            {
+                log.WarnFormat(messageFormat.Replace("{milliseconds}", "{0:N0}"), timespan.TotalMilliseconds);
+            }
+            else
+            {
+                log.WarnFormat(messageFormat, timespan);
+            }
+        }
+    }
+}
