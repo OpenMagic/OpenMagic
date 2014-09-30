7a51c87a91fdcea4de974532ca39406cf9ce57d4
Tim Murphy <tim@26tp.com>
2014-10-01 04:45:14 +1000

    Added UriExtensions.

---
+++ Projects/OpenMagic.Tests/Extensions/UriExtensionsTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using FluentAssertions;
+using OpenMagic.Extensions;
+using Xunit;
+
+namespace OpenMagic.Tests.Extensions
+{
+    public class UriExtensionsTests
+    {
+        public class GetResponseStatusCode
+        {
+            [Fact]
+            public void ShouldReturnHttpStatusCode()
+            {
+                // Given
+                var uri = new Uri("http://google.com");
+
+                // When
+                var statusCode = uri.GetResponseStatusCode();
+
+                // Then
+                statusCode.Should().Be(HttpStatusCode.OK, "because the world will have ended if '{0}' is down :-)", uri);
+            }
+        }
+
+        public class ResponseIsSuccessStatusCode
+        {
+            [Fact]
+            public void ShouldReturnTrueWhenWebsiteIsRunning()
+            {
+                // Given
+                var uri = new Uri("http://google.com");
+
+                // When
+                var responseIsOK = uri.ResponseIsSuccessStatusCode();
+
+                // Then
+                responseIsOK.Should().BeTrue("because the world will have ended if '{0}' is down :-)", uri);
+            }
+
+            [Fact]
+            public void ShouldReturnFalseWhenWebsiteDoesNotExist()
+            {
+                // Given
+                var uri = new Uri("http://a.a");
+
+                // When
+                var responseIsOK = uri.ResponseIsSuccessStatusCode();
+
+                // Then
+                responseIsOK.Should().BeFalse("because '{0}' should not exist", uri);
+            }
+        }
+    }
+}
---
+++ Projects/OpenMagic/Extensions/UriExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace OpenMagic.Extensions
+{
+    public static class UriExtensions
+    {
+        private static HttpResponseMessage GetResponse(this Uri uri)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                return httpClient.GetAsync(uri).Result;
+            }
+        }
+
+        public static HttpStatusCode GetResponseStatusCode(this Uri uri)
+        {
+            using (var response = uri.GetResponse())
+            {
+                return response.StatusCode;
+            }
+        }
+
+        public static bool ResponseIsSuccessStatusCode(this Uri uri)
+        {
+            try
+            {
+                using (var httpResponseMessage = uri.GetResponse())
+                {
+                    return httpResponseMessage.IsSuccessStatusCode;
+                }
+            }
+            catch (AggregateException aggregateException)
+            {
+                if (aggregateException.InnerException.GetType() == typeof(HttpRequestException))
+                {
+                    return false;
+                }
+                throw;
+            }
+        }
+    }
+}
