c300a2b3f3066bb3fe7e529b870a1aaff674a467
Tim Murphy <tim@26tp.com>
2015-01-28 04:17:54 +1100

    Updated copyright years.

--- Projects/OpenMagic/Properties/CommonAssemblyInfo.cs
+++ Projects/OpenMagic/Properties/CommonAssemblyInfo.cs
@@ -3,10 +3,9 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("OpenMagic")]
 [assembly: AssemblyCompany("Tim Murphy")]
-[assembly: AssemblyCopyright("Copyright © Tim Murphy 2013")]
+[assembly: AssemblyCopyright("Copyright © Tim Murphy 2013-2015")]
 [assembly: CLSCompliant(false)]
 
 // MyGet Build Services stamps the version number.
-
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
