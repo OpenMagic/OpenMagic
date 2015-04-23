ae739f000c27c969ed6727a833a8beda005f4666
Tim Murphy <tim@26tp.com>
2015-04-24 04:41:54 +1000

    Changed logging configuration to minimal information.

--- source/OpenMagic/Properties/AssemblyInfo.cs
+++ source/OpenMagic/Properties/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
+using Anotar.LibLog;
 
 [assembly: AssemblyTitle("OpenMagic")]
 [assembly: AssemblyDescription("Collection of utility extensions and classes by Tim Murphy.")]
+[assembly: LogMinimalMessage]
