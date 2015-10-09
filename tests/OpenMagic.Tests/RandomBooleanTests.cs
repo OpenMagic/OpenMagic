5637aaf8f85f4209e8b2ea9bfa33a98efcac51a8
Tim Murphy <tim@26tp.com>
2015-10-09 12:34:01 +1100

    Add RandomBoolean

---
+++ tests/OpenMagic.Tests/RandomBooleanTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace OpenMagic.Tests
+{
+    public class RandomBooleanTests
+    {
+        public class Next
+        {
+            [Fact]
+            public void ShouldGetRandomValues()
+            {
+                // When
+                var randomBooleans = RandomBoolean.Enumerable(1000);
+
+                // Then
+                randomBooleans.Count(b => b).Should().BeInRange(450, 550, "because approximately 50% of values should be true");
+            }
+        }
+    }
+}
