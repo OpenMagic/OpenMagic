b1806ed1cd0abcf7f8a8ae8b66cda517b38848f9
Tim Murphy <tim@26tp.com>
2016-01-16 06:11:45 +1100

    Add method to dispose of lazy value if value is created

---
+++ source/OpenMagic/Extensions/LazyExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OpenMagic.Extensions
+{
+    public static class LazyExtensions
+    {
+        public static void DisposeValueIfCreated<T>(Lazy<T> obj) where T : IDisposable
+        {
+            if (obj.IsValueCreated)
+            {
+                obj.Value.Dispose();
+            }
+        }
+    }
+}
