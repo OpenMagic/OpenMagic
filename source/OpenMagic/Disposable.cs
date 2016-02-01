4e574737dcd263a7be2642c357b44d8c21c54e02
Tim Murphy <tim@26tp.com>
2016-02-02 03:51:18 +1100

    Add abstract class that implements IDispoable pattern

---
+++ source/OpenMagic/Disposable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace OpenMagic
+{
+    public abstract class Disposable : IDisposable
+    {
+        private bool _isDisposed;
+
+        /// <summary>
+        ///     Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        ///     Releases unmanaged and - optionally - managed resources
+        /// </summary>
+        /// <param name="disposing">
+        ///     <c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.
+        /// </param>
+        private void Dispose(bool disposing)
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                DisposeManagedResources();
+            }
+            DisposeUnmanagedResources();
+
+            _isDisposed = true;
+        }
+
+        /// <summary>
+        ///     Releeases (disposes) managed resourcese
+        /// </summary>
+        protected virtual void DisposeManagedResources()
+        {
+        }
+
+        /// <summary>
+        ///     Releeases unmanaged resourcese
+        /// </summary>
+        protected virtual void DisposeUnmanagedResources()
+        {
+        }
+    }
+}
