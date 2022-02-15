6ccedb12fe9b09fa167a529c58d6f0326f9fbaf7
Tim Murphy <tim@26tp.com>
2022-02-16 07:39:42 +1100

    Update version number

--- source/OpenMagic/Properties/CommonAssemblyInfo.cs
+++ source/OpenMagic/Properties/CommonAssemblyInfo.cs
@@ -3,9 +3,8 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("OpenMagic")]
 [assembly: AssemblyCompany("Tim Murphy")]
-[assembly: AssemblyCopyright("Copyright © Tim Murphy 2013-2015")]
+[assembly: AssemblyCopyright("Copyright © Tim Murphy 2013-2022")]
 [assembly: CLSCompliant(false)]
 
-// MyGet Build Services stamps the version number.
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
