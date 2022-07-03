909bb1820e6254c44265bb59f08d6aa756cfbda5
Tim Murphy <tim@26tp.com>
2022-07-03 16:40:37 +1000

    Remove git clean as it may be what's holding up myget server

--- build/myget.ps1
+++ build/myget.ps1
@@ -4,7 +4,9 @@ $build = $(Resolve-Path $PSScriptRoot\build.ps1)
 # The following git clean will have not real effect on MyGet server.
 # What is will do is make developer's directory structure same as on
 # MyGet server.
-&git clean -d -X --force "$solutionFolder"
-Write-Host
+#Write-Host "Running git clean..."
+#&git clean -d -X --force "$solutionFolder"
+#Write-Host
 
+Write-Host "Invoking build script '$build'..."
 Invoke-Expression ".""$build"""
