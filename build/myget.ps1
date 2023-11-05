a083b4f53f80ddbcfd8a9ca61dacd07ba13691dd
Tim Murphy <tim@26tp.com>
2023-11-05 11:49:14 +1100

    Fix modulesPath for MyGet build server

--- build/myget.ps1
+++ build/myget.ps1
@@ -1,5 +1,26 @@
+function Get-ModulesPath {
+
+    $myDocuments = $([Environment]::GetFolderPath("MyDocuments"))
+
+    $modulesPath = Resolve-Path "$myDocuments\WindowsPowerShell\Modules" -ErrorAction SilentlyContinue
+
+    if ($null -ne $modulesPath) {
+        return $modulesPath        
+    }
+
+    $modulesPath = Resolve-Path "$myDocuments\PowerShell\Modules" -ErrorAction SilentlyContinue
+
+    if ($null -ne $modulesPath) {
+        return $modulesPath        
+    }
+
+    Get-ChildItem $myDocuments
+
+    throw "Could not find modules path in '$myDocuments'."
+}
+
 $buildScript = $(Resolve-Path $PSScriptRoot\build.ps1)
-$modulesPath = $(Resolve-Path "$([Environment]::GetFolderPath("MyDocuments"))\WindowsPowerShell\Modules\VSSetup")
+$modulesPath = Get-ModulesPath + "\VSSetup"
 $vsSetupZip = $(Resolve-Path $PSScriptRoot\VSSetup.zip)
 
 Write-Host "Expanding '$vsSetupZip' to '$modulesPath'..."
