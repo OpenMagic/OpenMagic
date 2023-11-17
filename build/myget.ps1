ae81f22f6e4131fa257502358dd44bcba49fb98c
Tim Murphy <tim@26tp.com>
2023-11-18 10:00:32 +1100

    temporary test on myget build server to see if VSSetup is required

--- build/myget.ps1
+++ build/myget.ps1
@@ -55,13 +55,16 @@ $buildScript = $(Resolve-Path $PSScriptRoot\build.ps1)
 $modulesPath = Get-ModulesPath + "\VSSetup"
 $vsSetupZip = $(Resolve-Path $PSScriptRoot\VSSetup.zip)
 
-Write-Host "Expanding '$vsSetupZip' to '$modulesPath'..."
-Expand-Archive $vsSetupZip $modulesPath
-Write-Host
-
-Write-Host "Installing VSSetup module..."
-Install-Module VSSetup -Scope CurrentUser -Force
-Write-Host
+# temporary test on myget build server to see if VSSetup is required
+if ($false) {
+    Write-Host "Expanding '$vsSetupZip' to '$modulesPath'..."
+    Expand-Archive $vsSetupZip $modulesPath
+    Write-Host
+    
+    Write-Host "Installing VSSetup module..."
+    Install-Module VSSetup -Scope CurrentUser -Force
+    Write-Host
+}
 
 Write-Host "Invoking build script '$buildScript'..."
 Invoke-Expression ".""$buildScript"""
