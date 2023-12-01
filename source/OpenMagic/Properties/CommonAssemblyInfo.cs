90793bea0f0315ede63ae50a2bc819f7ed844c09
Tim Murphy <tim@26tp.com>
2023-12-01 16:19:25 +1100

    Convert to visual studio 2022 (#9)
    * Update xUnit.* NuGet packages
    
    * Remove note about xUnit NuGet packages from README.md
    
    * Remove build-task.ps1 from solution file
    
    * Delete build.bat and remove build.bat from solution file
    
    * Fix xunit path
    
    * Update version number

--- source/OpenMagic/Properties/CommonAssemblyInfo.cs
+++ source/OpenMagic/Properties/CommonAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Tim Murphy 2013-2022")]
 [assembly: CLSCompliant(false)]
 
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyVersion("4.1.0.0")]
+[assembly: AssemblyFileVersion("4.1.0.0")]
