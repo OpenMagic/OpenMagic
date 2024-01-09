665f362b5229174a91d34c1f2c3416dd77192255
Tim Murphy <tim@26tp.com>
2024-01-10 01:47:14 +1100

    Update copyright years

--- source/OpenMagic/Properties/CommonAssemblyInfo.cs
+++ source/OpenMagic/Properties/CommonAssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("OpenMagic")]
 [assembly: AssemblyCompany("Tim Murphy")]
-[assembly: AssemblyCopyright("Copyright © Tim Murphy 2013-2022")]
+[assembly: AssemblyCopyright("Copyright © Tim Murphy 2013-2024")]
 [assembly: CLSCompliant(false)]
 
 [assembly: AssemblyVersion("4.3.0.0")]
