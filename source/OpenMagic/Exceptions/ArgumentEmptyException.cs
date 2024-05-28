76a9584963c2ed713b3a5e803b9d472e86340f33
Tim Murphy <tim@26tp.com>
2024-05-29 07:07:32 +1000

    Updated `Argument` class and replaced `NullGuard` namespace
    Updated the `Argument` class by moving it into the `OpenMagic` namespace and adding new methods for argument validation
    
    Additional comments were added to the `Argument` class for better code readability. Also, two new exception classes, `ArgumentEmptyException` and `ArgumentWhitespaceException`, were introduced for more specific exception handling.

---
+++ source/OpenMagic/Exceptions/ArgumentEmptyException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OpenMagic.Exceptions
+{
+    /// <summary>
+    ///     The exception that is thrown when an argument value is empty.
+    /// </summary>
+    public class ArgumentEmptyException : ArgumentException
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ArgumentEmptyException" /> class with the specified parameter name.
+        /// </summary>
+        /// <param name="paramName">The name of the parameter that caused the exception.</param>
+        public ArgumentEmptyException(string paramName)
+            : base("Value cannot be empty.", paramName.MustNotBeNullOrWhiteSpace(nameof(paramName)))
+        {
+        }
+    }
+}
