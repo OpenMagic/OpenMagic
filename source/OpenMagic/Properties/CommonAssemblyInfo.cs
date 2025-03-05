fa1a31654799840ee4820cc532ee2be2238cb35e
Tim Murphy <tim@26tp.com>
2025-03-05 16:40:25 +1100

    Update OpenMagic version to 5.7.1
    - Incremented version numbers in multiple files:
      `build-test-and-publish.yml`, `OpenMagic.csproj`,
      and `CommonAssemblyInfo.cs` to reflect the new
      version 5.7.1.

--- source/OpenMagic/Properties/CommonAssemblyInfo.cs
+++ source/OpenMagic/Properties/CommonAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Tim Murphy 2013-2024")]
 [assembly: CLSCompliant(false)]
 
-[assembly: AssemblyVersion("5.7.0.0")]
-[assembly: AssemblyFileVersion("5.7.0.0")]
+[assembly: AssemblyVersion("5.7.1.0")]
+[assembly: AssemblyFileVersion("5.7.1.0")]
